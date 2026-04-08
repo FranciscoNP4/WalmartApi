2eff9b1c1f0900a7ed13cc5f29074810842aba42
Francisco <franknajeraps4@gmail.com>
2026-04-07 17:53:20 -0700

    second commit

--- src/main/java/com/walmartapi/service/ProductService.java
+++ src/main/java/com/walmartapi/service/ProductService.java
@@ -1,18 +1,40 @@
 package com.walmartapi.service;
 
+import com.walmartapi.entity.ProductEntity;
 import com.walmartapi.model.Product;
+import com.walmartapi.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProductService {
 
-    public Product saveProduct(Product product) {
+    private ProductService productRepository;
 
+    public ProductService(ProductService productRepository) {
+        this.productRepository = productRepository;
+    }
+    public Product saveProduct(Product product) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setDescription(product.getDescription());
+        productEntity.setName(product.getName());
+        productEntity.setPrice(product.getPrice());
+
+        ProductEntity savedEntity = productRepository.save(productEntity);
         // map POJO to entity
         // Call DB
         // map entity to POJO
+        product.setName(savedEntity.getName());
+        product.setId(savedEntity.getId());
+        product.setPrice(product.getPrice());
+        product.setDescription(savedEntity.getDescription());
 
         return product;
     }
+
+    private ProductEntity save(ProductEntity productEntity) {
+        return productRepository.save(productEntity);
+    }
+
 
 }
